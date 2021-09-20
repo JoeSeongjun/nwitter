2a354186d547de4d72ec75d5bf9ab13a6b1d817e
JoeSeongjun <sungjun402@gamail.com>
2021-09-20 23:24:31 +0900

    1.0 v2

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+// import firebase from 'firebase/compat/app';
+// import firebase from "firebase/app";
+// import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfnlBmEoIVM4TTgnkM-NWY4wysxtEu1Vg",
+  authDomain: "nwitter-62bee.firebaseapp.com",
+  projectId: "nwitter-62bee",
+  storageBucket: "nwitter-62bee.appspot.com",
+  messagingSenderId: "343608681088",
+  appId: "1:343608681088:web:f40236fb2314b08e933c65",
+  measurementId: "G-GCT1QFMNC0"
+};
+
+export default initializeApp(firebaseConfig);
+// export default firebase.initializeApp(firebaseConfig);
